83b723cb6804a95e29003bec51e974cab8286098
biver25 <ya.ilya.burtsev@yandex.ru>
2021-05-25 00:08:27 +0300

    feature:class Popup created

---
+++ scripts/Popup.js
@@ -0,0 +1,31 @@
+export class Popup {
+  constructor(popupSelector) {
+    this._popup = document.querySelector(popupSelector);
+  }
+
+  open() {
+    this._popup.classList.remove('popup_disabled');
+    this._popup.classList.add('popup_opened');
+    document.addEventListener('keydown', this._handleEscClose);
+  }
+
+  close = () => {
+    this._popup.classList.add('popup_disabled');
+    this._popup.classList.remove('popup_opened');
+    document.removeEventListener('keydown', this._handleEscClose);
+  }
+
+  _handleEscClose(event) {
+    if (event.key === 'Escape') {
+      this.close();
+    }
+  }
+
+  setEventListeners() {
+    this._popup.querySelector('.popup__exit-btn')
+    .addEventListener('click', () => {
+      this.close();
+    })
+  }
+
+}
